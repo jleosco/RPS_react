c4c672b08bb0a9cc55ff73986ffc0bc0fc7dfb57
John Leosco <jrleosco@gmail.com>
2022-02-02 00:16:02 -0700

    rps working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react"
 
-function App() {
+const App = () => {
+  const [player, setPlayer] = useState(null)
+  const [computer, setComputer] = useState(null)
+  const [winner, setWinner] = useState('Start Game')
+
+  const options = ['rock', 'paper', 'scissors']
+
+  useEffect(()=>{
+    win()
+  }, [player])
+
+  const win = () => {
+    setComputer(options[Math.floor(Math.random()*3)])
+    if (player === null && computer === null){
+      setWinner('Start Game')
+    } else if (player === computer){
+        setWinner('Tie') 
+    } else if (player === options[0]){
+        if(computer === options[1]){
+          setWinner('Computer Wins')
+        } else{
+          setWinner('Player Wins')
+        }
+    } else if (player === options[1]){
+        if(computer === options[2]){
+          setWinner('Computer Wins')
+        } else{
+          setWinner('Player Wins')
+        }
+    } else if (player === options[2]){
+        if(computer === options[0]){
+          setWinner('Computer Wins')
+        } else{
+          setWinner('Player Wins')
+        }
+      }
+      setPlayer("")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Rock, Paper, Scissors</h1>
+      <div>
+        <button onClick={() => setPlayer(options[0])}>Rock</button>  
+        <button onClick={() => setPlayer(options[1])}>Paper</button>  
+        <button onClick={() => setPlayer(options[2])}>Scissors</button>  
+        <h3>{winner}</h3>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
